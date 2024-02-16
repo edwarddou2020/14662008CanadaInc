9c391757e3b74888566cc026f10ec44a39e8645d
edwarddou2020 <zuochao.dou@nvxclouds.com>
2024-02-16 12:35:37 -0500

    update content

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,17 +5,24 @@ function Services() {
         <h2 className="main-title text-center">SERVICES</h2>
         <div className="card-cover">
             <div className="col-md-12">
-                <div className="row">
-                    <div className="col-md-4 mb-2">
-                        <Card title="Web Development Solutions" img="card1.png" text="Web services design, development and maintenance. " />
-                    </div>
-                    <div className="col-md-4 mb-2">
-                    <Card title="Privacy Computing Solutions" img="card2.png" text="Trust Execution Environment, Federated Learning, secure multi-party computing." />
-                    </div>
+              <div className="row">
+                <div className="col-md-4 mb-2">
+                  <Card title="Web Development Solutions" img="card1.png"
+                        text="Web services design, development and maintenance. "/>
                 </div>
+                <div className="col-md-4 mb-2">
+                  <Card title="Privacy Computing Solutions" img="card3.png"
+                        text="Key management service, federated learning, secure multi-party computing."/>
+                </div>
+                <div className="col-md-4 mb-2">
+                  <Card title="Data Privacy Consulting" img="card2.png"
+                        text="Personal data security, large data secure transfer."/>
+                </div>
+              </div>
             </div>
         </div>
     </div>
   );
 }
+
 export default Services;
